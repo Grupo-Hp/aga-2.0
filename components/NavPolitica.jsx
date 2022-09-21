1cfa7b56cac0a88c0b05652b14f57a62106735fa
icaro_albar <icaro.albar@live.com>
2022-09-21 11:42:42 -0300

    Politica configurada

---
+++ components/NavPolitica.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+export default function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <>
+            <div className="shadow-sm py-7 w-full bg-primary-10 border-b z-50">
+                <div className="w-full container mx-auto">
+                    <div className="flex items-center h-20 w-full">
+                        <div className="flex items-center  mx-20  justify-between w-full">
+                            <div className="flex justify-center items-center flex-shrink-0 ">
+                                <a href="./"><Image
+                                    src="/images/logoAga.svg"
+                                    width={90}
+                                    height={90}
+                                    alt="Logo da AGÁ EMPREENDIMENTOS"
+
+                                /></a>
+                            </div>
+                            <div className="hidden lg:block">
+                                <div className="ml-10 flex items-baseline space-x-10 uppercase">
+                                    <a href="./"
+                                        className='hover:text-secondary-20 duration-300 rounded-md cursor-pointer text-white'
+                                    >
+                                        Página inicial
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import Page from '../components/Page'
+import PoliticaComponents from '../components/PoliticaComponets'
 
 export default function Home() {
 
   return (
     <Page title="AGÁ Empreendimentos" description="Site AGÁ Empreendimentos" path="/">
+      <PoliticaComponents />
     </Page>
   )
 }
