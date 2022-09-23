41d644c294e268bea97365092591b8ae682b6395
icaro_albar <icaro.albar@live.com>
2022-09-23 12:32:47 -0300

    Imagem header

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/header.module.css'
 export default function Header() {
     return (
         <div className={`h-[80vh] flex items-center border-b-8 border-secondary-20 ${styles.header}`}>
-            <div className="flex justify-center container mx-auto">
+            <div className="flex justify-center w-8/12 mx-auto">
                 <div className="grid grid-cols-2 gap-20">
                     <div className="hidden lg:col-span-1 lg:flex items-center order-2 md:order-1">
                         <Image src="/images/AGA_site_12.png" width="576" height="505" alt="Imagem ilustrativa" />
