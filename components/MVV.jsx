09b8c234045a5f78e085d3bddd6fc3f0aa0cec34
icaro_albar <icaro.albar@live.com>
2022-09-23 12:19:45 -0300

    reparo responsivel

--- components/MVV.jsx
+++ components/MVV.jsx
@@ -5,25 +5,25 @@ export default function MVV() {
     return (
         <div className={`${styles.mvv}`}>
 
-            <div className="container mx-auto hidden xl:grid grid-cols-3 gap-x-10 my-14">
+            <div className="w-9/12 mx-auto hidden xl:grid grid-cols-3 gap-x-14 my-14">
                 <div className="col-span-3 lg:col-span-1">
                     <h3>missão</h3>
                     <Image src="/images/missao.png" width="380" height="253" alt="Imagem ilustrativa" />
-                    <p className='w-4/5 leading-7'>
+                    <p className=' leading-7'>
                         Entregar produtos de excelência, respeitando todas as normas de Segurança, Qualidade e Meio ambiente, que superem as expectativas de nossos clientes e parceiros.
                     </p>
                 </div>
                 <div className="col-span-3 lg:col-span-1">
                     <h3>visão</h3>
                     <Image src="/images/visao.png" width="380" height="253" alt="Imagem ilustrativa" />
-                    <p className='w-4/5 leading-7'>
+                    <p className='leading-7'>
                         Ser referência no mercado de Construção Civil em comprometimento, ética profissional e qualidade, se consolidando como uma marca forte e única.
                     </p>
                 </div>
                 <div className="col-span-3 lg:col-span-1">
                     <h3>valores</h3>
                     <Image src="/images/valores.png" width="380" height="253" alt="Imagem ilustrativa" />
-                    <p className='w-4/5 leading-7'>
+                    <p className='leading-7'>
                         Ética, Transparência e Integridade, buscando sempre as melhores soluções de mercado e construindo relacionamento de longo prazo com nossos clientes, parceiros e colaboradores; Sustentabilidade e Responsabilidade Social, contribuindo sócio e economicamente para as comunidades em que atuamos; Inovação, buscando sempre soluções técnicas mais adequadas e economicamente mais favoráveis.
                     </p>
                 </div>
@@ -31,7 +31,7 @@ export default function MVV() {
 
             {/* MODO TABLET */}
 
-            <div className="container hidden md:grid gap-y-10 mx-auto my-14 xl:hidden">
+            <div className="w-8/12 hidden md:grid gap-y-10 mx-auto my-14 xl:hidden">
                 <div className="grid grid-cols-2 gap-x-10">
                     <div className="col-span-1">
                         <Image src="/images/missao.png" width="380" height="253" alt="Imagem ilustrativa" />
--- components/QuemSomos.jsx
+++ components/QuemSomos.jsx
@@ -4,20 +4,27 @@ import Image from 'next/image'
 export default function QuemSomos() {
     return (
         <div className={`${styles.quemSomos}`}>
-            <div className={`w-8/12 mx-auto py-10 gap-10 grid grid-cols-2`}>
-                <div className='flex items-center'>
-                    <div className="col-span-1 text-justify text-xl font-light leading-8">
-                        <h2 className={`text-secondary-10 uppercase text-3xl mb-3 font-semibold ${styles.footer}`}>quem somos</h2>
-                        <p className="mb-5">
-                            Somos a Agá Empreendimentos, referencial para quem busca refinamento e requinte arquitetônico. Estamos localizados em Icaraí, uma das áreas mais importantes da cidade de Niterói. Atuamos com Projetos, Reformas, Construção e Incorporação Imobiliária, tendo amplo destaque no cenário Fluminense.
-                        </p>
-                        <p>
-                            Nosso trabalho é baseado pela transparência, sendo pautado pela qualidade de vida. Buscamos sempre imprimir em nossos projetos o padrão Agá Empreendimentos, que é norteado pela sofisticação, elegância e aprumo técnico. A Agá Empreendimentos carrega o DNA da inovação e a busca pelo cuidado social e ambiental.
-                        </p>
+            <div className='w-8/12 mx-auto py-10'>
+                <div className={`grid lg:grid-cols-2 gap-10`}>
+                    <div className='flex items-center'>
+                        <div className="col-span-2 lg:col-span-1 md:text-xl font-light leading-8">
+                            <h2 className={`text-secondary-10 uppercase text-3xl mb-3 font-semibold ${styles.footer}`}>quem somos</h2>
+                            <p className="mb-5 order-1">
+                                Somos a Agá Empreendimentos, referencial para quem busca refinamento e requinte arquitetônico. Estamos localizados em Icaraí, uma das áreas mais importantes da cidade de Niterói. Atuamos com Projetos, Reformas, Construção e Incorporação Imobiliária, tendo amplo destaque no cenário Fluminense.
+                            </p>
+                            <p className='hidden xl:flex'>
+                                Nosso trabalho é baseado pela transparência, sendo pautado pela qualidade de vida. Buscamos sempre imprimir em nossos projetos o padrão Agá Empreendimentos, que é norteado pela sofisticação, elegância e aprumo técnico. A Agá Empreendimentos carrega o DNA da inovação e a busca pelo cuidado social e ambiental.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-span-2 lg:col-span-1 order-2">
+                        <Image src="/images/queSomos.png" width="865" height="929" alt="Imagem ilustrativa" />
                     </div>
                 </div>
-                <div className="col-span-1">
-                    <Image src="/images/queSomos.png" width="865" height="929" alt="Imagem ilustrativa" />
+                <div className='md:text-xl font-light leading-8 mt-5 xl:hidden order-3'>
+                    <p>
+                        Nosso trabalho é baseado pela transparência, sendo pautado pela qualidade de vida. Buscamos sempre imprimir em nossos projetos o padrão Agá Empreendimentos, que é norteado pela sofisticação, elegância e aprumo técnico. A Agá Empreendimentos carrega o DNA da inovação e a busca pelo cuidado social e ambiental.
+                    </p>
                 </div>
             </div>
         </div>
