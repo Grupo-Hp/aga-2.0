999e0a755d984c51958fc9af05674b14ceba0532
Icaro Albar <icaro.albar@live.com>
2022-09-28 14:20:51 -0300

    atualização de texto

--- components/ItaipuMall.jsx
+++ components/ItaipuMall.jsx
@@ -8,19 +8,25 @@ export default function ItaipuMall() {
         <div className={`${styles.quemSomos}`}>
             <div className="grid grid-cols-2">
                 <div className="col-span-2 xl:col-span-1 w-3/4 xl:w-3/4 2xl:w-2/4 m-auto py-10">
-                    <h2 className={`text-secondary-10 uppercase text-3xl mb-10 font-semibold ${styles.footer}`}>itaípu mall</h2>
+                    <h2 className={`text-secondary-10 uppercase text-3xl mb-10 font-semibold ${styles.footer}`}>itaipu mall</h2>
                     <p className='text-2xl text-justify font-light leading-10'>
                         Requinte e luxo transformados em arquitetura. Localizado em uma das áreas mais nobres de Niterói, o novo projeto da Agá Empreendimentos, o ITAIPU MALL, abre espaço para as maiores e mais importantes lojas do Brasil.
                     </p>
                     <p className='hidden lg:flex text-2xl text-justify font-light py-5 leading-10'>
                         Com cerca de 14 mil m², shopping terá mais de 20 estabelecimentos diversificados, dentre os quais: supermercado, academia, farmácia e pet shop. Além de um amplo estacionamento coberto, o ITAIPU MALL também contará com uma confortável praça de alimentação.
                     </p>
+                    <Link to="contato"
+                        smooth={true}
+                        offset={50}
+                        duration={1000}>
+                        <button type="" className="mt-24 w-full px-6 py-2.5 text-secondary-20 font-light text-2xs leading-tight uppercase border-2 border-secondary-20 hover:bg-secondary-20 hover:text-white transition duration-300 ease-in-out">descubra mais sobre esse empreendimento</button>
+                    </Link>
                 </div>
                 <div className="hidden xl:block col-span-1" id='portfolio'>
                     <Image src="/images/itaipu1.png" layout='responsive' width={100} height={100} alt="Imagem ilustrativa" />
                 </div>
             </div>
-            <div className="grid grid-cols-2 gap-10 w-4/5 mx-auto py-16">
+            {/* <div className="grid grid-cols-2 gap-10 w-4/5 mx-auto py-16">
                 <div className="col-span-2 xl:col-span-1">
                     <div className="max-w-lg mx-auto">
                         <Image src="/images/itaipu2.png" layout='responsive' width={100} height={100} alt="Imagem ilustrativa" />
@@ -43,7 +49,7 @@ export default function ItaipuMall() {
                         <button type="" className="mt-24 w-full px-6 py-2.5 text-secondary-20 font-light text-2xs leading-tight uppercase border-2 border-secondary-20 hover:bg-secondary-20 hover:text-white transition duration-300 ease-in-out">descubra mais sobre esse empreendimento</button>
                     </Link>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
