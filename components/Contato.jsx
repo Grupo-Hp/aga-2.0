55ba85d19c52103cd0183fe37c4616e94a67dc5f
Icaro Albar <icaro.albar@live.com>
2022-11-03 16:23:19 -0300

    React hook forms

--- components/Contato.jsx
+++ components/Contato.jsx
@@ -3,6 +3,9 @@ import { FiPhone } from "react-icons/fi";
 import { GoMail } from "react-icons/go";
 import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import styles from '../styles/footer.module.css'
+import { useForm } from "react-hook-form";
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup';
 
 
 export default function Contato() {
@@ -21,7 +24,6 @@ export default function Contato() {
     const onChangeInput = e => setDataForm({ ...dataForm, [e.target.name]: e.target.value })
 
     const sendContact = async e => {
-        e.preventDefault()
 
         try {
             await fetch('https://api.grupohp.com.br/send', {
@@ -34,6 +36,38 @@ export default function Contato() {
             alert('Ocorreu um erro')
         }
     }
+
+    const shape = yup.object().shape({
+        nome: yup
+            .string()
+            .required('Insira seu nome'),
+
+        email: yup
+            .string()
+            .email('Insira um e-mail válido')
+            .required('O e-mail é obrigatório'),
+
+        telefone: yup
+            .string()
+            .required('Isira um número de contato'),
+
+        assunto: yup
+            .string()
+            .required('Informe um assunto'),
+
+        mensagem: yup
+            .string()
+            .required('Digite sua mensagem'),
+    })
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(shape),
+        defaultValues: {
+            empresa: 'AGÁ Empreendimentos',
+            imagem: 'Aga',
+            site: 'https://www.agaempreendimentos.com.br/',
+        }
+    });
 
     return (
         <div className={`${styles.footer} ${styles.contato}`} id='contato'>
@@ -64,22 +98,27 @@ export default function Contato() {
 
                         <div className="col-span-2 xl:col-span-1">
                             <h4 className='text-3xl text-primary-10 pb-3 hidden xl:flex '>Fale conosco</h4>
-                            <form onSubmit={sendContact}>
+                            <form onSubmit={handleSubmit(sendContact)}>
 
                                 <div className="form-group mb-6">
 
-                                    <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" name="nome" onChange={onChangeInput} value={dataForm.nome} placeholder="Digite seu nome*" />
+                                    <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" {...register('nome')} onChange={onChangeInput} value={dataForm.nome} placeholder="Digite seu nome*" />
+                                    <p className="text-red-500 ml-3 mt-1">{errors?.nome?.message}</p>
 
                                 </div>
 
                                 <div className="form-group mb-6">
-                                    <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" name="email" onChange={onChangeInput} value={dataForm.email} placeholder="Digite seu e-mail*" />
+
+                                    <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" {...register('email')} onChange={onChangeInput} value={dataForm.email} placeholder="Digite seu e-mail*" />
+                                    <p className="text-red-500 ml-3 mt-1">{errors?.email?.message}</p>
+
                                 </div>
 
                                 <div className="grid grid-cols-2 gap-4">
                                     <div className="col-span-2 md:col-span-1 form-group mb-6">
 
-                                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" name="telefone" onChange={onChangeInput} value={dataForm.telefone} placeholder="Digite seu principal telefone*" />
+                                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" {...register('telefone')} onChange={onChangeInput} value={dataForm.telefone} placeholder="Digite seu principal telefone*" />
+                                        <p className="text-red-500 ml-3 mt-1">{errors?.telefone?.message}</p>
 
                                     </div>
 
@@ -87,14 +126,15 @@ export default function Contato() {
 
                                         <div className="flex">
                                             <div className="mb-3 w-full">
-                                                <select className="form-select block w-full px-3 py-1.5 text-base text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" aria-label="Default select example" name='assunto' value={dataForm.assunto} onChange={onChangeInput}>
-                                                    <option defaultValue>Escolhe o tipo de assunto:</option>
+                                                <select className="form-select block w-full px-3 py-1.5 text-base text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" aria-label="Default select example" {...register('assunto')} value={dataForm.assunto} onChange={onChangeInput}>
+                                                    <option defaultValue disabled>Escolhe o tipo de assunto:</option>
                                                     <option value="Comercial">Comercial</option>
                                                     <option value="Elogio">Elogio</option>
                                                     <option value="Reclamações">Reclamações</option>
                                                     <option value="Dúvidas">Dúvidas</option>
                                                     <option value="Sugestões">Sugestões</option>
                                                 </select>
+                                                <p className="text-red-500 ml-3 mt-1">{errors?.assunto?.message}</p>
                                             </div>
                                         </div>
                                     </div>
@@ -102,8 +142,8 @@ export default function Contato() {
 
                                 <div className="form-group mb-6">
 
-                                    <textarea className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" name="mensagem" rows="7" onChange={onChangeInput} value={dataForm.mensagem} placeholder="Escreva aqui..." required ></textarea>
-
+                                    <textarea className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary-20 focus:outline-none" {...register('mensagem')} rows="7" onChange={onChangeInput} value={dataForm.mensagem} placeholder="Escreva aqui..." ></textarea>
+                                    <p className="text-red-500 ml-3 mt-1">{errors?.mensagem?.message}</p>
                                     <button type="submit" className="mt-2 w-full px-6 py-2.5 text-primary-10 font-medium text-xs leading-tight uppercase border border-primary-20 hover:bg-primary-10 hover:text-white transition duration-300 ease-in-out">Enviar</button>
 
                                 </div>
