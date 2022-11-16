88e4c3c75a624a50645d1fe3abe91ff187fb055e
icaro_albar <icaro.albar@live.com>
2022-11-16 14:35:36 -0300

    Ocultar aruã

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import PoliticaComponents from '../components/PoliticaComponets'
 import Nav from '../components/Nav'
 import Header from '../components/Header'
 import ItaipuMall from '../components/ItaipuMall'
-import Arua from '../components/Arua'
+//import Arua from '../components/Arua'
 import QuemSomos from '../components/QuemSomos'
 import MVV from '../components/MVV'
 import Footer from '../components/Footer'
@@ -17,7 +17,6 @@ export default function Home() {
       <Nav />
       <Header />
       <ItaipuMall />
-      <Arua />
       <QuemSomos />
       <MVV />
       <Contato />
